a55c5f7d3b103ae8ab9421cce99ee75e14b66828
WonsonTheFantastic <wonson@gmail.com>
2020-03-12 15:36:46 +0800

    Version 2: Auto runloop
    HangSeng can auto-refresh and loop, no need to manually run anymore

--- HangSeng-eBanking-Template.js
+++ HangSeng-eBanking-Template.js
@@ -1,7 +1,7 @@
 // Usage:
 // 1. Fill the <SETUP YOUR INFO> section first
 // 2. Go to "PP支付平台"下的"繳付帳單"
-// 3. Run this in Google Chrome or Firefox console...by yourself after each refresh...
+// 3. Run this in Google Chrome (Tested) or Firefox (Not tested) console...
 
 // <SETUP YOUR INFO>
 
@@ -16,35 +16,52 @@ var __YOUR_DEBITACCT_CODE__ = "0";
 // </SETUP YOUR INFO>
 
 // CODE STARTS...Do not modify scripts below
-var step = 0;
-var stepFunc = [];
-stepFunc[0] = () => { 
-setPayeeRadioFocus(1);
-document.querySelector("input[id$=MerchantList-value]").value="0008";
-document.querySelector("input[name$=_BillAcct]").value=__YOUR_TAX_ID__;
-document.querySelector("input[name$=_PaymentAmt]").value=1;
-document.querySelector("input[id$=_DebitAcctList-value]").value = __YOUR_DEBITACCT_CODE__;
-refreshAccount();
-}
-stepFunc[1] = () => { document.querySelector("input[id$=_BillTypeList-value]").value="01";
-document.querySelector("a[id=okBtn]").click();
- };
-stepFunc[2] = () => {
-document.querySelector("div[class=btn-grn-1]").querySelector("a[onclick^=execute]").click();
-}
-stepFunc[3] = () => {
-goToRegPayeeListPage();
+var oDocRoot = document.removeChild(document.children[0]);
+var ifr = document.createElement("iframe");
+ifr.width = "100%"; 
+ifr.height = "100%"; 
+ifr.frameBorder = 0;
+document.appendChild(ifr);
+ifr.src = window.location;
+var contentWindow = ifr.contentWindow;
+
+function main() {
+
+	var step = 0;
+	var stepFunc = [];
+	stepFunc[0] = () => { 
+	contentWindow.setPayeeRadioFocus(1);
+	contentWindow.document.querySelector("input[id$=MerchantList-value]").value="0008";
+	contentWindow.document.querySelector("input[name$=_BillAcct]").value=__YOUR_TAX_ID__;
+	contentWindow.document.querySelector("input[name$=_PaymentAmt]").value=1;
+	contentWindow.document.querySelector("input[id$=_DebitAcctList-value]").value = __YOUR_DEBITACCT_CODE__;
+	contentWindow.refreshAccount();
+	}
+	stepFunc[1] = () => { contentWindow.document.querySelector("input[id$=_BillTypeList-value]").value="01";
+	contentWindow.document.querySelector("a[id=okBtn]").click();
+	 };
+	stepFunc[2] = () => {
+	contentWindow.document.querySelector("div[class=btn-grn-1]").querySelector("a[onclick^=execute]").click();
+	}
+	stepFunc[3] = () => {
+	contentWindow.goToRegPayeeListPage();
+	}
+
+	var stepFuncSelectors = [
+	()=> { var e = contentWindow.document.querySelector("input[id$=MerchantList-value]"); return contentWindow.utag_data.page_name_en.endsWith(":start") && (e != null) && (e.value!="0008"); },
+	()=> { var e = contentWindow.document.querySelector("input[id$=_BillTypeList-value]"); return contentWindow.utag_data.page_name_en.endsWith(":start") && (e != null) && (e.value!="01"); },
+	()=> { return contentWindow.utag_data.page_name_en.endsWith(":confirm"); },
+	()=> { return contentWindow.utag_data.page_name_en.endsWith(":acknowledgement"); },
+	];
+
+	for (var f of stepFuncSelectors) {
+	 if (f()) { break; } 
+	 step++;
+	};
+	if (step >= stepFunc.length)
+		throw new Error("Step cannot be determined."); 
+	else 
+		stepFunc[step]();
 }
 
-var stepFuncSelectors = [
-()=> { var e = document.querySelector("input[id$=MerchantList-value]"); return utag_data.page_name_en.endsWith(":start") && (e != null) && (e.value!="0008"); },
-()=> { var e = document.querySelector("input[id$=_BillTypeList-value]"); return utag_data.page_name_en.endsWith(":start") && (e != null) && (e.value!="01"); },
-()=> { return utag_data.page_name_en.endsWith(":confirm"); },
-()=> { return utag_data.page_name_en.endsWith(":acknowledgement"); },
-];
-
-for (var f of stepFuncSelectors) {
- if (f()) { break; } 
- step++;
-};
-stepFunc[step]();
+ifr.onload = main;
